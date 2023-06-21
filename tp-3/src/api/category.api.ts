035711e2a16dc6e3449ec8297f45b6d393240789
arf-hamza <hamza.arfaoui03@outlook.fr>
2023-06-21 03:29:56 -0400

    Update Api

--- tp-3/src/api/category.api.ts
+++ tp-3/src/api/category.api.ts
@@ -1,43 +1,69 @@
 
 
-export async function getApiCategory() {
-    const response = await fetch("https://tp-3-api.onrender.com/categories");
-    const jsonData = await response.json();
-    console.log(jsonData);
-    }
-
-export async function postApiCategory(){
-    const response = await fetch("https://tp-3-api.onrender.com/categories", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          name: "",
-        }),
-      });
-      const jsonData = await response.json();
-      console.log(jsonData);
+interface APICategory {
+  name: string;
+  id: string;
 }
 
-export async function putApiCategory(id){
-    const response = await fetch("https://tp-3-api.onrender.com/categories/" + id, {
+export async function getApiCategory() {
+  try {
+    const response = await fetch("https://tp-3-api.onrender.com/categories");
+    const jsonData = await response.json();
+
+    // Extraction de l'ID pour chaque catégorie
+    const categoryId = jsonData.map((category: APICategory) => category.id);
+    console.log("IDs des catégories :", categoryId);
+  } catch (error) {
+    console.error("Erreur lors de la récupération des catégories :", error);
+  }
+}
+
+export async function postApiCategory(body: APICategory) {
+  try {
+    const response = await fetch("https://tp-3-api.onrender.com/categories", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+    const jsonData = await response.json();
+    console.log("Réponse de l'API après l'ajout :", jsonData);
+  } catch (error) {
+    console.error("Erreur lors de l'ajout d'une catégorie :", error);
+  }
+}
+
+export async function putApiCategory(id: string, body: APICategory) {
+  try {
+    const response = await fetch(
+      "https://tp-3-api.onrender.com/categories/" + id,
+      {
         method: "PUT",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({
-          name: "",
-        }),
-      });
-      const jsonData = await response.json();
-      console.log(jsonData);
+        body: JSON.stringify(body),
+      }
+    );
+    const jsonData = await response.json();
+    console.log("Réponse de l'API après la mise à jour :", jsonData);
+  } catch (error) {
+    console.error("Erreur lors de la mise à jour d'une catégorie :", error);
+  }
 }
 
-export async function deleteApiCategory(id){
-    const response = await fetch("https://tp-3-api.onrender.com/categories/" + id, {
+export async function deleteApiCategory(id: string) {
+  try {
+    const response = await fetch(
+      "https://tp-3-api.onrender.com/categories/" + id,
+      {
         method: "DELETE",
-      });
-      const jsonData = await response.json();
-      console.log(jsonData);
+      }
+    );
+    const jsonData = await response.json();
+    console.log("Réponse de l'API après la suppression :", jsonData);
+  } catch (error) {
+    console.error("Erreur lors de la suppression d'une catégorie :", error);
+  }
 }
--- tp-3/src/api/product.api.ts
+++ tp-3/src/api/product.api.ts
@@ -1,52 +1,73 @@
+
+
+interface APIProduct {
+  title: string;
+  description: string;
+  price: number;
+  categoryId: string;
+  userId: string;
+  id: string; 
+}
+
 export async function getApiProducts() {
-  const response = await fetch("https://tp-3-api.onrender.com/products");
-  const jsonData = await response.json();
-  console.log(jsonData);
+  try {
+    const response = await fetch("https://tp-3-api.onrender.com/products");
+    const jsonData = await response.json();
+
+    // Extraction de l'ID pour chaque produit
+    const productId = jsonData.map((product: APIProduct) => product.id);
+    console.log("IDs des produits :", productId);
+  } catch (error) {
+    console.error("Erreur lors de la récupération des produits :", error);
+  }
 }
 
-
-
-export async function postApiProduct() {
-  const response = await fetch("https://tp-3-api.onrender.com/products", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      title: "",
-      description: "",
-      price: "",
-      categoryId: "",
-      userId: "",
-    }),
-  });
-  const jsonData = await response.json();
-  console.log(jsonData);
+export async function postApiProduct(body: APIProduct) {
+  try {
+    const response = await fetch("https://tp-3-api.onrender.com/products", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+    const jsonData = await response.json();
+    console.log("Réponse de l'API après l'ajout du produit :", jsonData);
+  } catch (error) {
+    console.error("Erreur lors de l'ajout d'un produit :", error);
+  }
 }
 
-export async function putApiProduct(id) {
-  const response = await fetch("https://tp-3-api.onrender.com/products/" + id, {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      title: "",
-      description: "",
-      price: "",
-      categoryId: "",
-      userId: "",
-    }),
-  });
-  const jsonData = await response.json();
-  console.log(jsonData);
+export async function putApiProduct(id: string, body: APIProduct) {
+  try {
+    const response = await fetch(
+      "https://tp-3-api.onrender.com/products/" + id,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+      }
+    );
+    const jsonData = await response.json();
+    console.log("Réponse de l'API après la mise à jour du produit :", jsonData);
+  } catch (error) {
+    console.error("Erreur lors de la mise à jour d'un produit :", error);
+  }
 }
 
-export async function deleteApiProduct(id) {
-  const response = await fetch("https://tp-3-api.onrender.com/products/" + id, {
-    method: "DELETE",
-  });
-  const jsonData = await response.json();
-  console.log(jsonData);
+export async function deleteApiProduct(id: string) {
+  try {
+    const response = await fetch(
+      "https://tp-3-api.onrender.com/products/" + id,
+      {
+        method: "DELETE",
+      }
+    );
+    const jsonData = await response.json();
+    console.log("Réponse de l'API après la suppression du produit :", jsonData);
+  } catch (error) {
+    console.error("Erreur lors de la suppression d'un produit :", error);
+  }
 }
-
