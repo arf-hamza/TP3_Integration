192fe39c7e3662a8f5ef37bef913edc81413fc5c
stella <stella.garon@outlook.com>
2023-06-27 15:51:12 -0400

    Added profile pictures
    (developer cards)

--- tp-3/src/components/organisms/developer-cards.tsx
+++ tp-3/src/components/organisms/developer-cards.tsx
@@ -9,11 +9,11 @@ import ProfileCard from '../molecules/profil-card/profil-card';
 export default function DeveloperCards() {
     return (
     <Box sx={{ display: "flex", flexWrap: 'wrap', justifyContent: "space-around", gap: "1rem", marginTop: 3 }}>
-        <ProfileCard name='Aline' img='/img/react.png' role='Designer/Développeuse'/>
-        <ProfileCard name='Jonathan' img='/img/react.png' role='Designer/Développeur'/>
-        <ProfileCard name='Hamza' img='/img/react.png' role='Designer/Développeur'/>
-        <ProfileCard name='Juliana' img='/img/react.png' role='Designer/Développeuse'/>
-        <ProfileCard name='Stella' img='/img/react.png' role='Designer/Développeuse'/>
+        <ProfileCard name='Aline' img='/img/aline.png' role='Designer/Développeuse'/>
+        <ProfileCard name='Jonathan' img='/img/jo.png' role='Designer/Développeur'/>
+        <ProfileCard name='Hamza' img='/img/hamza.png' role='Designer/Développeur'/>
+        <ProfileCard name='Juliana' img='/img/juliana.png' role='Designer/Développeuse'/>
+        <ProfileCard name='Stella' img='/img/stella.png' role='Designer/Développeuse'/>
     </Box>
   );
   }
